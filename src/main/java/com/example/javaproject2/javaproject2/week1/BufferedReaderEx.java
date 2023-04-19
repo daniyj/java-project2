465f0d688971bb70ad39fdc296544dbe0afb2e2d
dani <joyjoy312@naver.com>
2023-04-19 15:00:44 +0900

    feat: BufferedReaderEx, BufferedReaderExTest 추가

---
+++ src/main/java/com/example/javaproject2/javaproject2/week1/BufferedReaderEx.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.javaproject2.week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderEx {
+    public void readALine() throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
+
+        System.out.println("line1 = " + br.readLine());
+        System.out.println("Line2 = " + br.readLine());
+    }
+}
