3862a0899f919f6ec5f2c48aeb15472c0f673541
dani <joyjoy312@naver.com>
2023-04-19 15:39:43 +0900

    feat: ScannerEx, ScannerExTest 추가

---
+++ src/main/java/com/example/javaproject2/javaproject2/week1/ScannerExTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.javaproject2.week1;
+
+public class ScannerExTest {
+
+    public static void main(String[] args) {
+        ScannerEx se = new ScannerEx();
+        se.readTwoNumbersAndPlus();
+    }
+
+}
