45e9db9f49a562879832bc93256fce77207b622c
dani <joyjoy312@naver.com>
2023-04-20 15:43:02 +0900

    feat: DefineAndInitialize, ScannerAndVariable 추가 - 변수 선언과 초기화, 변수에 값을 담아 출력

---
+++ src/main/java/com/example/javaproject2/javaproject2/week1/DefineAndInitialize.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.javaproject2.week1;
+
+import com.example.javaproject2.javaproject2.PrintHello;
+
+import java.util.Scanner;
+
+public class DefineAndInitialize {
+    public static void main(String[] args) {
+        int iVal = 1;
+        PrintHello printHello = new PrintHello();
+        Scanner scanner = new Scanner(System.in);
+
+        Scanner sc;
+        sc = new Scanner(System.in);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/javaproject2/week1/ScannerAndVariable.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerAndVariable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int iVal = sc.nextInt();
+
+        System.out.println(iVal);
+
+    }
+}
