653b3332d3c611ed09e1df87448b827acc0a0845
dani <joyjoy312@naver.com>
2023-04-21 14:47:20 +0900

    feat: Point, PointTest 추가

---
+++ src/main/java/com/example/javaproject2/javaproject2/week1/day5/Point.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.javaproject2.week1.day5;
+
+public class Point {
+    int x;
+    int y;
+
+    boolean isSameXy(){
+        // x와 y가 같냐?
+        return x == y;
+    }
+
+    public static void main(String[] args) {
+        Point p = new Point();
+        System.out.println(p.x);
+    }
+}
