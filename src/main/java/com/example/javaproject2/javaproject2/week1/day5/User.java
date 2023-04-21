65dc0ad1f58d3654a1f2e92113417a8145654c59
dani <joyjoy312@naver.com>
2023-04-21 15:17:05 +0900

    feat: User, UserTest 추가

---
+++ src/main/java/com/example/javaproject2/javaproject2/week1/day5/User.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.javaproject2.week1.day5;
+
+public class User {
+    // 이름, 전화번호, 나이
+    String name;
+    String phoneNumber;
+    int age;
+
+}
---
+++ src/main/java/com/example/javaproject2/javaproject2/week1/day5/UserTest.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.javaproject2.week1.day5;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User kyeongrok = new User();
+        kyeongrok.name = "김경록";
+        kyeongrok.phoneNumber = "010-0000-1234";
+        kyeongrok.age = 37;
+
+        User jaesung = new User();
+        jaesung.name = "재성 매니저님";
+        jaesung.phoneNumber = "010-0000-1235";
+        jaesung.age = 24;
+
+    }
+}
