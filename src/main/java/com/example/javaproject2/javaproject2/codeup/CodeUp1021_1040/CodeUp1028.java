ef672831d2f5a338fe9457a0ce9a059cac272147
dani <joyjoy312@naver.com>
2023-04-24 15:29:43 +0900

    feat: Codeup1028-1030

--- src/main/java/com/example/javaproject2/javaproject2/codeup/CodeUp1021_1040/CodeUp1028.java
+++ src/main/java/com/example/javaproject2/javaproject2/codeup/CodeUp1021_1040/CodeUp1028.java
@@ -2,7 +2,7 @@ package com.example.javaproject2.javaproject2.codeup.CodeUp1021_1040;
 
 import java.util.Scanner;
 
-public class CodeUp1030 {
+public class CodeUp1028 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(sc.nextLong());
--- src/main/java/com/example/javaproject2/javaproject2/codeup/CodeUp1021_1040/CodeUp1029.java
+++ src/main/java/com/example/javaproject2/javaproject2/codeup/CodeUp1021_1040/CodeUp1029.java
@@ -1,4 +1,10 @@
 package com.example.javaproject2.javaproject2.codeup.CodeUp1021_1040;
 
+import java.util.Scanner;
+
 public class CodeUp1029 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%.11f",sc.nextDouble());
+    }
 }
