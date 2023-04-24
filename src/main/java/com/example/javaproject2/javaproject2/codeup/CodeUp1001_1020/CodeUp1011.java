5f89f4f89b73115bdc0b0545a4efe024df5c9a6c
dani <joyjoy312@naver.com>
2023-04-24 09:37:41 +0900

    feat: Codeup1011 다시추가

---
+++ src/main/java/com/example/javaproject2/javaproject2/codeup/CodeUp1001_1020/CodeUp1011.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.javaproject2.codeup.CodeUp1001_1020;
+
+import java.util.Scanner;
+
+public class CodeUp1011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next());
+
+    }
+}
