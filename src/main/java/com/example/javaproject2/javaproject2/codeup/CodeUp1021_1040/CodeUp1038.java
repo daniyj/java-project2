46cfbe29c5265367a029486eed54c8aff827a3de
dani <joyjoy312@naver.com>
2023-04-25 15:32:23 +0900

    feat: CodeUp1038-1040

---
+++ src/main/java/com/example/javaproject2/javaproject2/codeup/CodeUp1021_1040/CodeUp1038.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.javaproject2.codeup.CodeUp1021_1040;
+
+import java.util.Scanner;
+
+public class CodeUp1038 {
+    //정수 2개 입력받아 합 출력(int범위 넘어가서 long형 이용)
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Long num1 = sc.nextLong();
+        Long num2 = sc.nextLong();
+        System.out.printf("%d",num1 + num2);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/javaproject2/codeup/CodeUp1021_1040/CodeUp1040.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.javaproject2.codeup.CodeUp1021_1040;
+
+import java.util.Scanner;
+
+public class CodeUp1040 {
+    public static void main(String[] args) {
+        //정수 1개 입력받아 부호 바꿔 출력하기
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%d",-sc.nextInt());
+    }
+}
