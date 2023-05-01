801a7c8b87d69f22934f63827c8e3c0732af07e9
dani <joyjoy312@naver.com>
2023-05-01 13:42:52 +0900

    feat: LoopAscii, LoopChar, LoopCharInt 추가

---
+++ src/main/java/com/example/javaproject2/javaproject2/week3/LoopAscii.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.javaproject2.week3;
+
+public class LoopAscii {
+    public static void main(String[] args) {
+        for(int c = 65; c <= '2';c++){
+            System.out.printf("%d: %c\n",c ,c);
+        }
+    }
+}
---
+++ src/main/java/com/example/javaproject2/javaproject2/week3/LoopChar.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.javaproject2.week3;
+
+public class LoopChar {
+    public static void main(String[] args) {
+//        for(char c = 'a'; c <= 'z';c++){
+//            System.out.println(c);
+//        }
+        //한글 loop
+        for(char c = '가'; c <= '힣';c++){
+            System.out.printf("%c ", c);
+        }
+    }
+}
