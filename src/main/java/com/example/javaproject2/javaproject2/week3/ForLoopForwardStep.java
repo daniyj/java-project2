91616e6b4c793bc0a921013b4d0fb7fbd930287b
dani <joyjoy312@naver.com>
2023-05-01 13:27:27 +0900

    feat: ForLoopForwardStep 추가

---
+++ src/main/java/com/example/javaproject2/javaproject2/week3/ForLoopForwardStep.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.javaproject2.week3;
+
+public class ForLoopForwardStep {
+    public static void main(String[] args) {
+        //짝수 출력
+        for(int i = 0;i < 10;i +=2){
+            System.out.println(i);
+        }
+        //홀수 출력
+        for(int i = 1;i < 10;i+=2){
+            System.out.println(i);
+        }
+    }
+}
