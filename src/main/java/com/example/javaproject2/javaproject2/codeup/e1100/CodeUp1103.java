68b377929a928f02a9df42d97fca6c04b821df6a
dani <joyjoy312@naver.com>
2023-05-01 11:02:09 +0900

    feat: CodeUp1103, 1106

---
+++ src/main/java/com/example/javaproject2/javaproject2/codeup/e1100/CodeUp1103.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.javaproject2.codeup.e1100;
+
+public class CodeUp1103 {
+    public static void main(String[] args) {
+        System.out.println("\"c:\\test\"");
+    }
+}
---
+++ src/main/java/com/example/javaproject2/javaproject2/codeup/e1100/CodeUp1106.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.javaproject2.codeup.e1100;
+
+public class CodeUp1106 {
+    //int형의 최소값, 최대값 출력하기
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE+" "+Integer.MAX_VALUE);
+    }
+}
