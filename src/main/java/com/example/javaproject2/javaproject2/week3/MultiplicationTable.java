692c98a0eaac9f31a96ace8684254e298e41cde0
dani <joyjoy312@naver.com>
2023-05-01 14:20:23 +0900

    feat: MultiplicationTable 추가

---
+++ src/main/java/com/example/javaproject2/javaproject2/week3/MultiplicationTable.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.javaproject2.week3;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for(int i = 1;i <= 9;i++){
+            System.out.printf("%d\n", 2 * i);
+        }
+    }
+}
